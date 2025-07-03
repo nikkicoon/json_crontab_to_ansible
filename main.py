43e69f6e6800dedc7005389cdc1d070f95ae8d98
Ronja Nikita Gillmann <ronja@klang.is>
2025-07-03 18:48:37 +0200

    skip default values

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import datetime
 import json
 import re
+import sys
+
 import click
 
 """
@@ -15,13 +17,13 @@ ansible.builtin.cron:
 """
 
 @click.command()
-@click.option("--infile", default="crontabs.json")
+@click.option("--infile", type=click.File('r'), default=sys.stdin)
 @click.option("--outfile", default="ansible_from_json.yml")
 @click.option("--cronfile", default="foo")
 def main(infile: str, outfile: str, cronfile: str):
     n = int(datetime.datetime.timestamp(datetime.datetime.now()))
     outfile = outfile + "." + str(n)
-    with open(infile, "r") as f:
+    with infile as f:
         data = json.load(f)
         # print(data)
         if "schedule" in data:
@@ -33,14 +35,14 @@ def main(infile: str, outfile: str, cronfile: str):
                     header = "name: EDITME\nansible.builtin.cron:"
                     cro = "\tcron_file: " + cronfile
                     user = "\tuser: " + command_parts[0]
-                    minute = "\tminute: " + "".join(k["minute"])
-                    hour = "\thour: " + "".join(k["hour"])
-                    month = "\tmonth: " + "".join(k["month"])
-                    day = "\tday: " + "".join(k["day_of_month"])
-                    weekday = "\tweekday: " + "".join(k["day_of_week"])
+                    minute = "\tminute: " + "".join(k["minute"]) if k["minute"] != ["*"] else None
+                    hour = "\thour: " + "".join(k["hour"]) if k["hour"] != ["*"] else None
+                    month = "\tmonth: " + "".join(k["month"]) if k["month"] != ["*"] else None
+                    day = "\tday: " + "".join(k["day_of_month"]) if k["day_of_month"] != ["*"] else None
+                    weekday = "\tweekday: " + "".join(k["day_of_week"]) if k["day_of_week"] != ["*"] else None
                     cronname = "\tname: DESCRIBEME"
                     job = "\tjob: " + "\"" + " ".join(command_parts[1:]) + "\"" + "\n"
-                    res = "\n".join((header, cro, user, minute, hour, month, day, weekday, cronname, job))
+                    res = "\n".join(filter(None, (header, cro, user, minute, hour, month, day, weekday, cronname, job)))
                     print(res, file=o)
 
 if __name__ == "__main__":
