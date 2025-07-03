ddac153446f90280ba371abd018d60a52434edc8
Ronja Nikita Gillmann <ronja@klang.is>
2025-07-03 19:12:31 +0200

    check values

--- main.py
+++ main.py
@@ -5,45 +5,61 @@ import sys
 
 import click
 
-"""
-name: foobar|ARG
-ansible.builtin.cron:
-    cron_file: ARG
-    user: str(command[0])
-    minute: int
-    hour: int
-    name: description
-    job: str(command[1:])
-"""
 
 @click.command()
 @click.option("--infile", type=click.File('r'), default=sys.stdin)
 @click.option("--outfile", default="ansible_from_json.yml")
 @click.option("--cronfile", default="foo")
-def main(infile: str, outfile: str, cronfile: str):
+@click.option("--user", default="root")
+def main(infile: str, outfile: str, cronfile: str, user: str):
+    """outputs an ansible entry.
+
+    \b
+    name: foobar|ARG
+    ansible.builtin.cron:
+        cron_file: ARG
+        user: str(command[0])
+        minute: int
+        hour: int
+        name: description
+        job: str(command[1:])
+"""
     n = int(datetime.datetime.timestamp(datetime.datetime.now()))
     outfile = outfile + "." + str(n)
     with infile as f:
         data = json.load(f)
         # print(data)
         if "schedule" in data:
-            for k in data["schedule"]:
-                with open(outfile, "a") as o:
-                    command: str = k["command"]
-                    command_parts = re.split(r"\s+", command)
-                    # print(command_parts)
+            with open(outfile, "a") as o:
+                for k in data["schedule"]:
+                    if "command" in k:
+                        command: str = k["command"]
+                        command_parts = re.split(r"\s+", command)
+                        # print(command_parts)
                     header = "name: EDITME\nansible.builtin.cron:"
                     cro = "\tcron_file: " + cronfile
-                    user = "\tuser: " + command_parts[0]
-                    minute = "\tminute: " + "".join(k["minute"]) if k["minute"] != ["*"] else None
-                    hour = "\thour: " + "".join(k["hour"]) if k["hour"] != ["*"] else None
-                    month = "\tmonth: " + "".join(k["month"]) if k["month"] != ["*"] else None
-                    day = "\tday: " + "".join(k["day_of_month"]) if k["day_of_month"] != ["*"] else None
-                    weekday = "\tweekday: " + "".join(k["day_of_week"]) if k["day_of_week"] != ["*"] else None
+                    if len(command_parts) > 0:
+                        user = "\tuser: " + command_parts[0]
+                    else:
+                        user = " ".join(("\tuser:", user))
+                    if "minute" in k:
+                        minute = "\tminute: " + "".join(k["minute"]) if k["minute"] != ["*"] else None
+                    if "hour" in k:
+                        hour = "\thour: " + "".join(k["hour"]) if k["hour"] != ["*"] else None
+                    if "month" in k:
+                        month = "\tmonth: " + "".join(k["month"]) if k["month"] != ["*"] else None
+                    if "day_of_month" in k:
+                        day = "\tday: " + "".join(k["day_of_month"]) if k["day_of_month"] != ["*"] else None
+                    if "day_of_week" in k:
+                        weekday = "\tweekday: " + "".join(k["day_of_week"]) if k["day_of_week"] != ["*"] else None
                     cronname = "\tname: DESCRIBEME"
-                    job = "\tjob: " + "\"" + " ".join(command_parts[1:]) + "\"" + "\n"
+                    if len(command_parts) > 1:
+                        job = "\tjob: " + "\"" + " ".join(command_parts[1:]) + "\"" + "\n"
+                    else:
+                        job = None
                     res = "\n".join(filter(None, (header, cro, user, minute, hour, month, day, weekday, cronname, job)))
                     print(res, file=o)
 
+
 if __name__ == "__main__":
-    main()
+    main(max_content_width=120)
